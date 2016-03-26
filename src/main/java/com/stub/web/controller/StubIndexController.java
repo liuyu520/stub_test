50798ca9d53ae58069005dbe43794197d7fafa0d
huangweii <huangweii@chanjet.com>
2016-03-26 20:51:29 +0800

    继承oa_framework中的 StubIndexController

---
+++ src/main/java/com/stub/web/controller/StubIndexController.java
@@ -0,0 +1,13 @@
+package com.stub.web.controller;
+
+/**
+ * Created by whuanghkl on 3/26/16.
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/stubIndex")
+public class StubIndexController extends oa.web.controller.common.StubIndexController {
+}
